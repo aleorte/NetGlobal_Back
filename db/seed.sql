2708797dceab05a0ab0038207ad2d2bbdd22194b
PauQuiriti <pauquiriti@hotmail.com>
2022-06-23 18:10:35 -0300

    seed2

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,4 @@
 
-INSERT INTO admins (email) VALUES ('admin@admin.com')
-INSERT INTO guards (email, nombre, apellido, CUIL) VALUES ('paula@hotmail.com',"Paula","Quiriti","27-37389294-2")
-INSERT INTO guards (email, nombre, apellido, CUIL) VALUES ('cristian@hotmail.com',"Cristian","Diaz","20-33382290-2")
-INSERT INTO guards (email, nombre, apellido, CUIL) VALUES ('marco@hotmail.com',"Marco","Cabrera","20-47333980-2")
+ INSERT INTO admins (email,password) VALUES ('admin@admin.com','1234')
+ INSERT INTO guards (name, lastName, cuil, email, password, street, number,location,coordinateLatitude,coordinateLength,providenceId) VALUES ('Paula','Quiriti','27-37389294-2','pauquiriti@hotmail.com','12345', 'Valencia',5888,'Mar del Plata', 41.40 , 21.17, 4)
+ INSERT INTO guards (name, lastName, cuil, email, password, street, number,location,coordinateLatitude,coordinateLength,provinceId) VALUES ('Paul','Jones','20-3730094-2','paul@hotmail.com','99', 'Tierra del Fuego',1999,'Santa Clara ', 45.25 , 2.17, 1)
