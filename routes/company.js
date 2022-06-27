76fd94a5c76a5c8f3e5e575c4b33fe8a048bafd7
PauQuiriti <pauquiriti@hotmail.com>
2022-06-27 13:14:16 -0300

    controllers  y services de company

--- routes/company.js
+++ routes/company.js
@@ -2,24 +2,22 @@ const { Router } = require('express');
 const express = require('express');
 const companyRouter = express.Router();
 const {Company}= require ('../models')
+const CompanyController = require('../controllers/companyController')
 const findAllActiveCompanies = require('../controllers/findAllActiveCompanies')
-const findAllCompanies = require('../controllers/findAllCompanies')
-const createCompany = require('../controllers/createCompany')
-const findCompany = require('../controllers/findCompany')
-const modifyCompany = require('../controllers/modifyCompany');
 const findInactiveCompanies = require('../controllers/findInactiveCompanies');
 
+
 //encuentra todas las companies 
-companyRouter.get('/', findAllCompanies)
+companyRouter.get('/', CompanyController.getAll)
 //encuentra todas las companies activas 
-companyRouter.get('/active', findAllActiveCompanies)
+companyRouter.get('/active', CompanyController.getActiveOnes)
 // encuentra todas las companies inactivas 
-companyRouter.get('/inactive',findInactiveCompanies)
+companyRouter.get('/inactive',CompanyController.getInactiveOnes)
 // crea una nueva company 
-companyRouter.post('/', createCompany)
+companyRouter.post('/', CompanyController.addOne)
 //solo  encuentra la company  y avisa si esta inactiva 
-companyRouter.get('/:id', findCompany)
+companyRouter.get('/:id', CompanyController.getOne)
 //modifica una company
-companyRouter.put('/:id', modifyCompany)
+companyRouter.put('/:id', CompanyController.updateOne)
 
 module.exports = companyRouter
