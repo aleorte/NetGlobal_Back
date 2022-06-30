ce0b88f5922a2af2655784ba98600970e15b8a97
PauQuiriti <pauquiriti@hotmail.com>
2022-06-30 12:28:43 -0300

    fiked login fail

--- controllers/adminLoginController.js
+++ controllers/adminLoginController.js
@@ -4,7 +4,8 @@ class AdminLoginController{
 static async login(req,res){
     try{
         const {error,data} = await AdminLoginServices.login(req.body)
-          if (data) res.send(data)
+          if (error) res.status(data.code).send(data)
+          res.status(200).send(data)
     }
     catch(error){
       res.status(500).send(data)
